b755cc6e85862327d76dfe79116c01c3d97795ee
Alexey Korban <ldwx@mail.ru>
2018-11-01 17:07:14 +0500

    Added annotation for JPA

---
+++ src/main/java/local/ldwx/accounting/repository/jpa/JpaUserRepositoryImpl.java
@@ -0,0 +1,44 @@
+package local.ldwx.accounting.repository.jpa;
+
+import local.ldwx.accounting.model.User;
+import local.ldwx.accounting.repository.UserRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@Transactional(readOnly = true)
+public class JpaUserRepositoryImpl implements UserRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Override
+    @Transactional
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        return false;
+    }
+
+    @Override
+    public User get(int id) {
+        return null;
+    }
+
+    @Override
+    public User getByEmail(String email) {
+        return null;
+    }
+
+    @Override
+    public List<User> getAll() {
+        return null;
+    }
+}
