fbef12dd149bf59c15493d41a7cc33545daf3d06
AlexeyKorban <ldwx@mail.ru>
2018-11-10 15:47:22 +0500

    Add jdbc project and user service tests

---
+++ src/test/java/local/ldwx/accounting/service/jdbc/JdbcProjectServiceTest.java
@@ -0,0 +1,10 @@
+package local.ldwx.accounting.service.jdbc;
+
+import local.ldwx.accounting.service.AbstractProjectServiceTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static local.ldwx.accounting.Profiles.JDBC;
+
+@ActiveProfiles(JDBC)
+public class JdbcProjectServiceTest extends AbstractProjectServiceTest {
+}
