1fe8b1d883a7cf15b309066dfed7ab4c50cecc34
Alexey Korban <ldwx@mail.ru>
2018-11-23 14:54:12 +0500

    Remove servlet, add Root controller

---
+++ src/main/java/local/ldwx/accounting/web/RootController.java
@@ -0,0 +1,46 @@
+package local.ldwx.accounting.web;
+
+import local.ldwx.accounting.service.ProjectService;
+import local.ldwx.accounting.service.UserService;
+import local.ldwx.accounting.util.ProjectsUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class RootController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping("/")
+    public String root() {
+        return "index";
+    }
+
+    @GetMapping("/users")
+    public String users(Model model) {
+        model.addAttribute("users", userService.getAll());
+        return "users";
+    }
+
+    @PostMapping("/users")
+    public String setUser(HttpServletRequest request) {
+        int userId = Integer.valueOf(request.getParameter("userId"));
+        SecurityUtil.setAuthUserId(userId);
+        return "redirect:projects";
+    }
+
+    @GetMapping("/projects")
+    public String projects(Model model) {
+        model.addAttribute("projects",
+                ProjectsUtil.getWithExcess(projectService.getAll(SecurityUtil.authUserId()), SecurityUtil.authUserSumPerDay()));
+        return "projects";
+    }
+}
