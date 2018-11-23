86c87f03114e922b1f99ffa41323bf326ec0a63a
Alexey Korban <ldwx@mail.ru>
2018-11-23 14:02:49 +0500

    Add getRootCause into Validation util

--- src/main/java/local/ldwx/accounting/util/ValidationUtil.java
+++ src/main/java/local/ldwx/accounting/util/ValidationUtil.java
@@ -9,6 +9,9 @@ public class ValidationUtil {
         return checkNotFound(object, "id=" + id);
     }
 
+    private ValidationUtil() {
+    }
+
     public static void checkNotFoundWithId(boolean found, int id) {
         checkNotFound(found, "id=" + id);
     }
@@ -37,5 +40,16 @@ public class ValidationUtil {
         } else if (entity.getId() != id) {
             throw new IllegalArgumentException(entity + " must be with id=" + id);
         }
+    }
+
+    //  http://stackoverflow.com/a/28565320/548473
+    public static Throwable getRootCause(Throwable t) {
+        Throwable result = t;
+        Throwable cause;
+
+        while(null != (cause = result.getCause()) && (result != cause)) {
+            result = cause;
+        }
+        return result;
     }
 }
