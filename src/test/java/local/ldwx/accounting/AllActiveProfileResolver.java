4f44135277b03452e2f5aaae5621d668f0bc1186
AlexeyKorban <ldwx@mail.ru>
2018-11-24 20:34:02 +0500

    Add root controller test

---
+++ src/test/java/local/ldwx/accounting/AllActiveProfileResolver.java
@@ -0,0 +1,11 @@
+package local.ldwx.accounting;
+
+import org.springframework.test.context.ActiveProfilesResolver;
+
+public class AllActiveProfileResolver implements ActiveProfilesResolver {
+
+    @Override
+    public String[] resolve(Class<?> aClass) {
+        return new String[]{Profiles.REPOSITORY_IMPLEMENTATION, Profiles.getActiveDbProfile()};
+    }
+}
---
+++ src/test/java/local/ldwx/accounting/web/RootControlerTest.java
@@ -0,0 +1,32 @@
+package local.ldwx.accounting.web;
+
+import org.junit.Test;
+
+import static local.ldwx.accounting.UserTestData.USER;
+import static local.ldwx.accounting.model.AbstractBaseEntity.START_SEQ;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.hasProperty;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
+public class RootControlerTest extends AbstractControllerTest {
+
+    @Test
+    public void testUsers() throws Exception {
+        mockMvc.perform(get("/users"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(view().name("users"))
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
+                .andExpect(model().attribute("users", hasSize(2)))
+                .andExpect(model().attribute("users", hasItem(
+                        allOf(
+                                hasProperty("id", is(START_SEQ)),
+                                hasProperty("name", is(USER.getName()))
+                        )
+                )));
+    }
+}
