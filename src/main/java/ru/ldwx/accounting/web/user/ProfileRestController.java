6bbae18ee107ab8fbc061b6393b824496a1a751b
AlexeyKorban <ldwx@mail.ru>
2018-11-28 20:34:02 +0500

    Add rest controller

--- src/main/java/ru/ldwx/accounting/web/user/ProfileRestController.java
+++ src/main/java/ru/ldwx/accounting/web/user/ProfileRestController.java
@@ -1,21 +1,30 @@
 package ru.ldwx.accounting.web.user;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 import ru.ldwx.accounting.model.User;
-import org.springframework.stereotype.Controller;
 
 import static ru.ldwx.accounting.web.SecurityUtil.authUserId;
 
-@Controller
-public class ProfileRestController extends AbstractUserController{
+@RestController
+@RequestMapping(ProfileRestController.REST_URL)
+public class ProfileRestController extends AbstractUserController {
+    static final String REST_URL = "/rest/profile";
 
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public User get() {
         return super.get(authUserId());
     }
 
+    @DeleteMapping
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete() {
         super.delete(authUserId());
     }
 
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void update(User user) {
         super.update(user, authUserId());
     }
