6a580b1f77b3ec7bc0ddcf10e28ea29f27883560
AlexeyKorban <ldwx@mail.ru>
2018-12-25 07:53:26 +0500

    Edit datatable via ajax

--- src/main/java/ru/ldwx/accounting/web/RootController.java
+++ src/main/java/ru/ldwx/accounting/web/RootController.java
@@ -1,20 +1,17 @@
 package ru.ldwx.accounting.web;
 
-import ru.ldwx.accounting.service.ProjectService;
-import ru.ldwx.accounting.service.UserService;
-import ru.ldwx.accounting.util.ProjectsUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import ru.ldwx.accounting.service.ProjectService;
+import ru.ldwx.accounting.util.ProjectsUtil;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class RootController {
-    @Autowired
-    private UserService userService;
 
     @Autowired
     private ProjectService projectService;
@@ -25,8 +22,7 @@ public class RootController {
     }
 
     @GetMapping("/users")
-    public String users(Model model) {
-        model.addAttribute("users", userService.getAll());
+    public String users() {
         return "users";
     }
 
