716c1542c0e72b8cb574f9473ae4e38418a4f089
AlexeyKorban <ldwx@mail.ru>
2019-01-01 16:16:49 +0500

    Fix RootControllerTest

--- src/test/java/ru/ldwx/accounting/web/RootControllerTest.java
+++ src/test/java/ru/ldwx/accounting/web/RootControllerTest.java
@@ -1,17 +1,14 @@
 package ru.ldwx.accounting.web;
 
-import org.assertj.core.matcher.AssertionMatcher;
 import org.junit.jupiter.api.Test;
-import ru.ldwx.accounting.model.User;
-
-import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static ru.ldwx.accounting.ProjectTestData.PROJECTS;
 import static ru.ldwx.accounting.TestUtil.userAuth;
-import static ru.ldwx.accounting.UserTestData.*;
+import static ru.ldwx.accounting.UserTestData.ADMIN;
+import static ru.ldwx.accounting.UserTestData.USER;
 import static ru.ldwx.accounting.util.ProjectsUtil.getWithExcess;
 
 public class RootControllerTest extends AbstractControllerTest {
@@ -23,14 +20,7 @@ public class RootControllerTest extends AbstractControllerTest {
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(view().name("users"))
-                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"))
-                .andExpect(model().attribute("users",
-                        new AssertionMatcher<List<User>>() {
-                            @Override
-                            public void assertion(List<User> actual) throws AssertionError {
-                                assertMatch(actual, ADMIN, USER);
-                            }
-                        }));
+                .andExpect(forwardedUrl("/WEB-INF/jsp/users.jsp"));
     }
 
     @Test
