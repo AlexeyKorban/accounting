c688cfce5cfddc2159f4a6131dc6849de3d759ed
AlexeyKorban <ldwx@mail.ru>
2019-01-09 07:45:11 +0500

    Add secure tag annotation

--- src/main/java/ru/ldwx/accounting/web/RootController.java
+++ src/main/java/ru/ldwx/accounting/web/RootController.java
@@ -1,5 +1,6 @@
 package ru.ldwx.accounting.web;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +13,7 @@ public class RootController {
         return "redirect:projects";
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping("/users")
     public String users() {
         return "users";
