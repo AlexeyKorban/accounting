8a2b38a86413a16a87e7e3e5b219ef6916052c13
AlexeyKorban <ldwx@mail.ru>
2019-01-14 08:01:52 +0500

    Add global controller exception handler

---
+++ src/main/java/ru/ldwx/accounting/web/GlobalControllerExceptionHandler.java
@@ -0,0 +1,31 @@
+package ru.ldwx.accounting.web;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import ru.ldwx.accounting.AuthorizedUser;
+import ru.ldwx.accounting.util.ValidationUtil;
+
+import javax.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class GlobalControllerExceptionHandler {
+    private static final Logger log = LoggerFactory.getLogger(GlobalControllerExceptionHandler.class);
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView defaultErrorHandler(HttpServletRequest req, Exception e) throws Exception {
+        log.error("Exception at request " + req.getRequestURL(), e);
+        ModelAndView mav = new ModelAndView("exception/exception");
+        Throwable rootCase = ValidationUtil.getRootCause(e);
+        mav.addObject("exception", rootCase);
+        mav.addObject("message", rootCase.toString());
+
+        AuthorizedUser authorizedUser = SecurityUtil.safeGet();
+        if (authorizedUser != null) {
+            mav.addObject("userTo", authorizedUser.getUserTo());
+        }
+        return mav;
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/exception/exception.jsp
@@ -0,0 +1,32 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Loky
+  Date: 14.01.2019
+  Time: 8:00
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page isErrorPage="true" contentType="text/html" pageEncoding="UTF-8" %>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<html>
+<jsp:include page="../fragments/headTag.jsp"/>
+
+<body>
+<jsp:include page="../fragments/bodyHeader.jsp"/>
+
+<div class="jumbotron">
+    <div class="container text-center">
+        <br>
+        <h2><spring:message code="common.appError"/></h2>
+        <h3>${message}</h3>
+    </div>
+</div>
+<!--
+<c:forEach items="${exception.stackTrace}" var="stackTrace">
+    ${stackTrace}
+</c:forEach>
+-->
+<jsp:include page="../fragments/footer.jsp"/>
+</body>
+</html>
