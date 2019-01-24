f2e8d2a657033ef8c1fcebab7230ccc7ae365ec4
AlexeyKorban <ldwx@mail.ru>
2019-01-24 07:53:00 +0500

    Add locale change

--- src/main/java/ru/ldwx/accounting/web/MessageUtil.java
+++ src/main/java/ru/ldwx/accounting/web/MessageUtil.java
@@ -3,10 +3,13 @@ package ru.ldwx.accounting.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
+@Component
 public class MessageUtil {
+    public static final Locale RU_LOCALE = new Locale("ru");
 
     private final MessageSource messageSource;
 
