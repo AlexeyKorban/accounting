a773946abb1caaca89f39318299d234d8599ff48
AlexeyKorban <ldwx@mail.ru>
2019-02-02 20:14:03 +0500

    Add heroku files

---
+++ hr.bat
@@ -0,0 +1,3 @@
+call mvn -B -s settings.xml -DskipTests=true clean package
+rem call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/accounting" -jar target/dependency/webapp-runner.jar target/*.war
+call java -Dspring.profiles.active="datajpa,heroku" -DDATABASE_URL="postgres://user:password@localhost:5432/accounting" -classpath target/dependency webapp.runner.launch.Main target/*.war
