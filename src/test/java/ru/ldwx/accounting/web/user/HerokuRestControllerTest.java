3765175e22057b5fe3ea76c2f217399e8ed3ee53
AlexeyKorban <ldwx@mail.ru>
2019-02-03 15:48:55 +0500

    Fix search i18n

--- src/test/java/ru/ldwx/accounting/web/user/HerokuRestControllerTest.java
+++ src/test/java/ru/ldwx/accounting/web/user/HerokuRestControllerTest.java
@@ -1,11 +1,17 @@
 package ru.ldwx.accounting.web.user;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.core.env.PropertySource;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import ru.ldwx.accounting.UserTestData;
 import ru.ldwx.accounting.util.exception.ErrorType;
 import ru.ldwx.accounting.web.AbstractControllerTest;
+
+import java.io.IOException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
@@ -19,9 +25,25 @@ import static ru.ldwx.accounting.UserTestData.USER_ID;
 import static ru.ldwx.accounting.util.exception.ModificationRestrictionException.EXCEPTION_MODIFICATION_RESTRICTION;
 
 @ActiveProfiles({HEROKU})
-public class HerokuRestControllerTest extends AbstractControllerTest {
+class HerokuRestControllerTest extends AbstractControllerTest {
 
     private static final String REST_URL = AdminRestController.REST_URL + '/';
+
+    static {
+        Resource resource = new ClassPathResource("db/postgres.properties");
+        try {
+            PropertySource propertySource = new ResourcePropertySource(resource);
+            String herokuDbUrl = String.format("postgres://%s:%s@%s",
+                    propertySource.getProperty("database.username"),
+                    propertySource.getProperty("database.password"),
+                    ((String) propertySource.getProperty("database.url")).substring(18));
+            System.out.println(herokuDbUrl);
+
+            System.setProperty("DATABASE_URL", herokuDbUrl);
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+    }
 
     @Test
     void testDelete() throws Exception {
